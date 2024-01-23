6254596ff516248207f981d6dd3d7f14df682bfe
LuisGRR <lgerardo685@gmail.com>
2024-01-23 02:15:35 -0600

    Se agrego campo update_at, para guardar la feha que se modifico

--- src/models/image.js
+++ src/models/image.js
@@ -11,6 +11,12 @@ const imageSchema = new Schema({
   width: { type: String },
   size: { type: Number },
   created_at: { type: Date, default: Date.now() },
+  update_at:{ type: Date, default: Date.now() },
+});
+
+imageSchema.pre('save', function (next) {
+  this.update_at = Date.now();
+  next();
 });
 
 module.exports = model("Image", imageSchema);
