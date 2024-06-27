d3bdcbeae070b22a4ee5b8ac9b0dc54aa90b6c00
LuisGRR <lgerardo685@gmail.com>
2024-06-27 16:04:52 -0600

    formateo de codigo

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -2,20 +2,20 @@
 const supertest = require("supertest");
 const userModel = require("../src/models/user.model");
 const userService = require("../src/services/userService");
-const {app,server} = require("../src/index");
+const { app, server } = require("../src/index");
 const { default: mongoose } = require("mongoose");
 const api = supertest(app)
 
 
-describe('API /register', () => { 
+describe('API /register', () => {
     beforeEach(async () => {
         await userModel.deleteMany({})
     });
-    test("Register user",async () =>{
-        const response = await api.post("/register").send({ name: 'gerardo3', password: 'gerardo3',email:'test3@test.com' }).expect('Content-Type', /application\/json/);
+    test("Register user", async () => {
+        const response = await api.post("/register").send({ name: 'gerardo3', password: 'gerardo3', email: 'test3@test.com' }).expect('Content-Type', /application\/json/);
         expect(response.status).toBe(201);
         expect(response.body.ok).toBe(true);
-    }); 
+    });
 
     /*test("Register error, whit non password",async () =>{
         const response = await api.post("/register").send({ name: '', password: 'gerardo3',email:'test3@test.com' });
@@ -26,12 +26,12 @@ describe('API /register', () => {
     */
 });
 
-describe('API /register NUM_USERS_SYS', () => { 
-    beforeEach(async function(){
+describe('API /register NUM_USERS_SYS', () => {
+    beforeEach(async function () {
         const initUser = [
-            { name: 'gerardo', password: 'gerardo1',email:'test@test.com' },
-            { name: 'gerardo2', password: 'gerardo2',email:'test2@test.com' },
-            { name: 'gerardo3', password: 'gerardo3',email:'test3@test.com' },
+            { name: 'gerardo', password: 'gerardo1', email: 'test@test.com' },
+            { name: 'gerardo2', password: 'gerardo2', email: 'test2@test.com' },
+            { name: 'gerardo3', password: 'gerardo3', email: 'test3@test.com' },
         ];
 
         await userModel.deleteMany({})
@@ -40,8 +40,8 @@ describe('API /register NUM_USERS_SYS', () => {
         const promises = userObjects.map(user => user.save());
         await Promise.all(promises)
     });
-    test("Register limit NUM_USERS_SYS",async () =>{
-        const response = await api.post("/register").send({ name: 'gerardo4', password: 'gerardo4',email:'test4@test.com' });
+    test("Register limit NUM_USERS_SYS", async () => {
+        const response = await api.post("/register").send({ name: 'gerardo4', password: 'gerardo4', email: 'test4@test.com' });
         expect(response.status).toBe(400);
         expect(response.body.ok).toBe(false);
         expect(response.body.err.message).toEqual("Existen mas ususarios de los configurados");
@@ -50,47 +50,47 @@ describe('API /register NUM_USERS_SYS', () => {
 });
 
 
-describe('API /login"',() => {
+describe('API /login"', () => {
     beforeEach(async () => {
         await userModel.deleteMany({})
-        await userService.saveUser( 'ExistentUser','ExistentPassword','test@test.com');
+        await userService.saveUser('ExistentUser', 'ExistentPassword', 'test@test.com');
     });
 
-    test("Login succesful",async () =>{
-       const response = await api.post("/login").send({ name: 'ExistentUser', password: 'ExistentPassword' }).expect('Content-Type', /application\/json/);
-       expect(response.status).toBe(200);
-       expect(response.body.ok).toBe(true);
+    test("Login succesful", async () => {
+        const response = await api.post("/login").send({ name: 'ExistentUser', password: 'ExistentPassword' }).expect('Content-Type', /application\/json/);
+        expect(response.status).toBe(200);
+        expect(response.body.ok).toBe(true);
     });
 
-    test("Login incorrect credentials", async () =>{
-        const response = await api.post("/login").send({ name:'nonExistentUser', password:'nonExistentPassword' });
+    test("Login incorrect credentials", async () => {
+        const response = await api.post("/login").send({ name: 'nonExistentUser', password: 'nonExistentPassword' });
         expect(response.status).toBe(400);
         expect(response.body.ok).toBe(false);
-       // expect(response.body.err.message).toEqual("Contraseña incorrecta");
+        // expect(response.body.err.message).toEqual("Contraseña incorrecta");
     });
 
-    test("Login with password incorrect",async () =>{
-        const response = await api.post("/login").send({ name:'ExistentUser', password:'nonExistentPassword' });
+    test("Login with password incorrect", async () => {
+        const response = await api.post("/login").send({ name: 'ExistentUser', password: 'nonExistentPassword' });
         expect(response.status).toBe(400);
         expect(response.body.ok).toBe(false);
         expect(response.body.err.message).toEqual("Contraseña incorrecta");
-     });
+    });
 
-    test("Login with non-existent user",async () =>{
+    test("Login with non-existent user", async () => {
         const response = await api.post("/login").send({ name: 'nonExistentUser', password: 'ExistentPassword' });
         expect(response.status).toBe(400);
         expect(response.body.ok).toBe(false);
         expect(response.body.err.message).toEqual("El Usuario no existe");
-     });
+    });
 });
 
-describe('API /logout', () => { 
-    beforeEach(async function(){
+describe('API /logout', () => {
+    beforeEach(async function () {
         await userModel.deleteMany({})
-        await userService.saveUser( 'ExistentUser','ExistentPassword','test@test.com');
+        await userService.saveUser('ExistentUser', 'ExistentPassword', 'test@test.com');
         await api.post("/login").send({ name: 'ExistentUser', password: 'ExistentPassword' });
     });
-    test("Logout",async () =>{
+    test("Logout", async () => {
         const response = await api.get("/logout");
         expect(response.status).toBe(302);
         expect(response.header.location).toBe('/');
@@ -101,12 +101,12 @@ describe('API /logout', () => {
         const response = await api.get('/logout');
         expect(response.status).toBe(302);
         expect(response.header.location).toBe('/');
-      });
+    });
 });
 
 
-afterAll(async ()=>{
+afterAll(async () => {
     await userModel.deleteMany({})
     await mongoose.connection.close()
     server.close();
-  });
+});
--- __tests__/tags.routers.spec.js
+++ __tests__/tags.routers.spec.js
@@ -3,77 +3,77 @@ const supertest = require("supertest");
 const tagsModel = require("../src/models/tags.model");
 const userModel = require("../src/models/user.model");
 const tagsService = require("../src/services/tagService");
-const {app,server} = require("../src/index");
+const { app, server } = require("../src/index");
 const { default: mongoose } = require("mongoose");
 const api = supertest(app)
 
 
-describe("Api /tag POST", ()=>{
+describe("Api /tag POST", () => {
     let cookie;
 
     beforeEach(async () => {
         await tagsModel.deleteMany({});
         await userModel.deleteMany({});
 
-        const registerResponse = await api.post("/register").send({ name: 'ExistentUser', password: 'ExistentPassword',email:'test789@test.com' });
+        const registerResponse = await api.post("/register").send({ name: 'ExistentUser', password: 'ExistentPassword', email: 'test789@test.com' });
         cookie = registerResponse.headers['set-cookie'];
     });
 
-    test("Tags POST succesful", async () =>{
+    test("Tags POST succesful", async () => {
         const response = await api.post("/tag")
-        .set('Cookie', cookie) // Establecer la cookie de sesión en la solicitud
-        .send({tags:['tags1', 'tag2']})
+            .set('Cookie', cookie) // Establecer la cookie de sesión en la solicitud
+            .send({ tags: ['tags1', 'tag2'] })
             .expect('Content-Type', /application\/json/);
         expect(response.status).toBe(200);
         expect(response.body.ok).toBe(true);
         expect(response.body.message).toBe("El recurso ha sido guardado exitosamente")
     });
 
-    test("Tags POST error send string", async () =>{
+    test("Tags POST error send string", async () => {
         const response = await api.post("/tag")
-        .set('Cookie', cookie) // Establecer la cookie de sesión en la solicitud
-        .send({tags:"esto no es una tag"});
+            .set('Cookie', cookie) // Establecer la cookie de sesión en la solicitud
+            .send({ tags: "esto no es una tag" });
         expect(response.status).toBe(400);
         expect(response.body.ok).toBe(false)
         expect(response.body.err.message).toBe("Tags should be an array")
     });
 
-    test("Tags POST, not array", async () =>{
+    test("Tags POST, not array", async () => {
         const response = await api.post("/tag")
-        .set('Cookie', cookie) // Establecer la cookie de sesión en la solicitud
-        .send({});
+            .set('Cookie', cookie) // Establecer la cookie de sesión en la solicitud
+            .send({});
         expect(response.status).toBe(400);
         expect(response.body.ok).toBe(false)
         expect(response.body.err.message).toBe("Tags should be an array")
     });
 
-    test("Tags POST array empty", async () =>{
+    test("Tags POST array empty", async () => {
         const response = await api.post("/tag")
-        .set('Cookie', cookie) // Establecer la cookie de sesión en la solicitud
-        .send({tags:[]});
+            .set('Cookie', cookie) // Establecer la cookie de sesión en la solicitud
+            .send({ tags: [] });
         expect(response.status).toBe(400);
         expect(response.body.ok).toBe(false);
         expect(response.body.err.message).toBe("The tag array is empty");
     });
 });
 
-describe("Api /tags GET", ()=>{
+describe("Api /tags GET", () => {
     let cookie;
 
     beforeEach(async () => {
         await tagsModel.deleteMany({});
         await userModel.deleteMany({});
 
-        const registerResponse = await api.post("/register").send({ name: 'ExistentUser', password: 'ExistentPassword',email:'test789@test.com' });
+        const registerResponse = await api.post("/register").send({ name: 'ExistentUser', password: 'ExistentPassword', email: 'test789@test.com' });
         cookie = registerResponse.headers['set-cookie'];
     });
 
-    test("Get tags with a 200 status code",async () =>{
+    test("Get tags with a 200 status code", async () => {
         const response = await api.get("/tags").set('Cookie', cookie).expect(200).expect('Content-Type', /application\/json/);
         expect(response.body).toEqual([]);
     });
 
-    test("Get tags errors",async () =>{
+    test("Get tags errors", async () => {
         await new tagsModel({ name: 'tag1' }).save();
         await new tagsModel({ name: 'tag2' }).save();
 
@@ -101,9 +101,9 @@ describe("Api /tags GET", ()=>{
 });
 
 
-afterAll(async ()=>{
+afterAll(async () => {
     await tagsModel.deleteMany({});
     await userModel.deleteMany({});
     await mongoose.connection.close()
     server.close();
-  });
+});
